66a824d128a931f99c85fc3459ef8e4a280a542d
talakquliks <128274306+talakquliks@users.noreply.github.com>
2025-06-19 00:29:07 +0900

    Update mmaatthhddll.py
    방정식의 해가 실수임에도 허수로 표현되는 오류 수정

--- mmaatthhddll.py
+++ mmaatthhddll.py
@@ -1,4 +1,4 @@
-import cmath
+import cmath, math
 
 print("ax^2 + bx + c = 0 형태의 방정식을 풀겠습니다.")
 a = float(input("x^2의 계수의 값을 입력하세요: "))
@@ -10,8 +10,8 @@ if D == 0:
     x = -b / (2 * a)  # 중근
     print(f"방정식의 해는 중근인 {x}입니다.")
 elif D > 0:
-    x1 = (-b + cmath.sqrt(D)) / (2 * a)
-    x2 = (-b - cmath.sqrt(D)) / (2 * a)
+    x1 = (-b + math.sqrt(D)) / (2 * a)
+    x2 = (-b - math.sqrt(D)) / (2 * a)
     print(f"방정식의 해는 {x1}와 {x2}입니다.")
 else:
     real_part = -b / (2 * a)
