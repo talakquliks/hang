17f6eccf1d8115edc5ba8513732364e399c1c8ae
talakquliks <128274306+talakquliks@users.noreply.github.com>
2025-06-20 09:22:38 +0900

    mmaatthhddll.py 업데이트
    a,b,c 값을 받았을 떄 이차함수가 성립되지 않으면 다시 시작하는 방법으로 함수를 재귀하는 방법을 사용했으나 오류가 난 뒷부분이 호출 스택이 쌓여 오류 로그가 호출되는 것을 방지하기 위해 solve()함수 안에 while 반복문을 추가하고 오류가 날 때마다 return으로 재귀하지 않아도 되게 하고 구 근이 나왔을 때는 break로 반복을 중단시켰다.

--- mmaatthhddll.py
+++ mmaatthhddll.py
@@ -2,32 +2,34 @@ import cmath, math
 
 def solve():
 
-    try:
-        a = float(input("x^2의 계수의 값을 입력하세요: "))
-        b = float(input("x의 계수의 값을 입력하세요: "))
-        c = float(input("상수항의 값을 입력하세요: "))
-    except Exception:
-        print("입력값을 확인하세요. 숫자만 입력 가능합니다. 다시 시도하세요.")
-        solve()
-    if a == 0:
-        print("이차방정식이 성립하지 않습니다.다시 시도하세요")
-        solve()
+    while True:
+        try:
+            a = float(input("x^2의 계수의 값을 입력하세요: "))
+            b = float(input("x의 계수의 값을 입력하세요: "))
+            c = float(input("상수항의 값을 입력하세요: "))
+        except Exception:
+            print("입력값을 확인하세요. 숫자만 입력 가능합니다. 다시 시도하세요.")
+            continue
+        if a == 0:
+            print("이차방정식이 성립하지 않습니다.다시 시도하세요")
+            continue
 
-    D = b**2 - 4*a*c  # 판별식 계산
+        D = b**2 - 4*a*c  # 판별식 계산
 
-    if D == 0:
-        x = -b / (2 * a)  # 중근
-        print(f"방정식의 해는 중근인 {x}입니다.")
-    elif D > 0:
-        x1 = (-b + math.sqrt(D)) / (2 * a)
-        x2 = (-b - math.sqrt(D)) / (2 * a)
-        print(f"방정식의 해는 {x1}와 {x2}입니다.")
-    else:
-        real_part = -b / (2 * a)
-        imaginary_part = cmath.sqrt(-D) / (2 * a)
-        x1 = complex(real_part, imaginary_part)
-        x2 = complex(real_part, -imaginary_part)
-        print(f"방정식의 해는 허수인 {x1}와 {x2}입니다.")
+        if D == 0:
+            x = -b / (2 * a)  # 중근
+            print(f"방정식의 해는 중근인 {x}입니다.")
+        elif D > 0:
+            x1 = (-b + math.sqrt(D)) / (2 * a)
+            x2 = (-b - math.sqrt(D)) / (2 * a)
+            print(f"방정식의 해는 {x1}와 {x2}입니다.")
+        else:
+            real_part = -b / (2 * a)
+            imaginary_part = cmath.sqrt(-D) / (2 * a)
+            x1 = complex(real_part, imaginary_part)
+            x2 = complex(real_part, -imaginary_part)
+            print(f"방정식의 해는 허수인 {x1}와 {x2}입니다.")
+        break    
         
 def r_continue():
     r = input("계속하시겠습니까? (y/n): ")
