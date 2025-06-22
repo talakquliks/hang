7cc72d4230a1331c9a6fc81be0594999ac36e310
talakquliks <128274306+talakquliks@users.noreply.github.com>
2025-06-22 12:35:37 +0900

    mmaatthhddll.py 업데이트 while 반복문 삭제 및 실용성 항목에서 오류 발생시 에러메세지 출력기능 추가

--- mmaatthhddll.py
+++ mmaatthhddll.py
@@ -1,47 +1,50 @@
 import cmath, math
 
 def solve():
-
-    while True:
-        try:
-            a = float(input("x^2의 계수의 값을 입력하세요: "))
-            b = float(input("x의 계수의 값을 입력하세요: "))
-            c = float(input("상수항의 값을 입력하세요: "))
-        except Exception:
-            print("입력값을 확인하세요. 숫자만 입력 가능합니다. 다시 시도하세요.")
-            continue
+    try:
+        #  a,b,c값 입력
+        a = int(input("x^2의 계수를 입력해 주세요.:"))
+        b = int(input("x의 계수를 입력해 주세요.:"))
+        c = int(input("상수항을 입력해 주세요.:"))
         if a == 0:
-            print("이차방정식이 성립하지 않습니다.다시 시도하세요")
-            continue
-
-        D = b**2 - 4*a*c  # 판별식 계산
-
+            print("a의 값이 0이 되면 이차방정식이 성립되지 않습니다.")
+            r_return()
+            return
+            
+        #판별식
+        D = b**2 - 4*a*c
+        #근의 형식 탐색
         if D == 0:
-            x = -b / (2 * a)  # 중근
-            print(f"방정식의 해는 중근인 {x}입니다.")
-        elif D > 0:
-            x1 = (-b + math.sqrt(D)) / (2 * a)
-            x2 = (-b - math.sqrt(D)) / (2 * a)
-            print(f"방정식의 해는 {x1}와 {x2}입니다.")
+            x = -b/(2*a)
+            print(f"이 방정식의 해는 중근으로 {x} 입니다.")
+        elif D >0:
+            x1 = (-b + math.sqrt(D))/(2*a)
+            x2 = (-b - math.sqrt(D))/(2*a)
+            print(f"이 방정식의 해는 {x1}, {x2} 입니다.")            
         else:
-            real_part = -b / (2 * a)
-            imaginary_part = cmath.sqrt(-D) / (2 * a)
-            x1 = complex(real_part, imaginary_part)
-            x2 = complex(real_part, -imaginary_part)
-            print(f"방정식의 해는 허수인 {x1}와 {x2}입니다.")
-        break    
-        
-def r_continue():
-    r = input("계속하시겠습니까? (y/n): ")
-    if r.lower() == 'y':
+            x1 = (-b + cmath.sqrt(D))/(2*a)
+            x2 = (-b - cmath.sqrt(D))/(2*a)
+            print(f"이 방정식의 해는 허근인 {x1}, {x2} 입니다.")
+    except ValueError:
+        print("숫자만 입력 가능합니다. 값을 확앤해 주세요")
+        r_return()
+        return
+    except Exception:
+        print("예기치 못한 오류가 발생하였습니다. 다시 시도해 주세요.")
+        r_return()
+    r_return()  
+            
+            
+def r_return():
+    answer = input("프로그램을 다시 시작할까요? (y/n)").strip()
+    if answer.lower() == "y":
         solve()
-        r_continue()
-    elif r.lower() == 'n':
+    elif answer.lower() == "n":
         print("프로그램을 종료합니다.")
     else:
-        print("잘못된 입력입니다. y 또는 n을 입력하세요.")
-
-print("ax^2 + bx + c = 0 형태의 방정식을 풀겠습니다.")
-solve()
-r_continue()
-
+        print("잘못된 입력입니다. 입력을 검토해 주세요.")
+        r_return()
+            
+            
+print("a^2 + bx + c = 0 꼴의 이차방정식의 근을 구하는 프로그램을 실행하겠습니다.")            
+solve()        
